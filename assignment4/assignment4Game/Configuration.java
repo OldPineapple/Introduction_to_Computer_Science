4e589b6b340aed04413e9ad059416b6333738f80
OldPineapple <46538736+OldPineapple@users.noreply.github.com>
2021-02-12 15:52:20 -0500

    Add files via upload

--- assignment4/assignment4Game/Configuration.java
+++ assignment4/assignment4Game/Configuration.java
@@ -1,5 +1,5 @@
 package assignment4Game;
-
+//Name: Hanwen Wang   ID:260778557
 public class Configuration {
 	
 	public int[][] board;
@@ -28,24 +28,254 @@ public class Configuration {
 			System.out.println();
 		}
 	}
-	
 	public void addDisk (int index, int player){
 		// ADD YOUR CODE HERE
+		if(index>=0&&index<=6&&available[index]<6)
+		{
+			board[index][available[index]]=player;
+			available[index]++;
+		}
+		for(int j=0;j<7;j++)
+		{
+			int num=0;
+			if(available[j]==6)
+			{
+				num++;
+			}
+			if(num==7)
+			{
+				spaceLeft=false;
+			}
+		}
 	}
 	
 	public boolean isWinning (int lastColumnPlayed, int player){
 		// ADD YOUR CODE HERE
-		return false; // DON'T FORGET TO CHANGE THE RETURN
+		//if(board[lastColumnPlayed][available[lastColumnPlayed]-1]!=player)
+		//	return false;
+		if(	lastColumnPlayed>=3&&lastColumnPlayed<=6&&
+				available[lastColumnPlayed]>=1&&available[lastColumnPlayed]<=6&&
+			board[lastColumnPlayed-1][available[lastColumnPlayed]-1]==player&&
+			board[lastColumnPlayed-2][available[lastColumnPlayed]-1]==player&&
+			board[lastColumnPlayed-3][available[lastColumnPlayed]-1]==player)
+		{
+			return true;
+		}
+		else if(lastColumnPlayed>=2&&lastColumnPlayed<=5&&
+				available[lastColumnPlayed]>=1&&available[lastColumnPlayed]<=6&&
+				board[lastColumnPlayed-1][available[lastColumnPlayed]-1]==player&&
+				board[lastColumnPlayed-2][available[lastColumnPlayed]-1]==player&&
+				board[lastColumnPlayed+1][available[lastColumnPlayed]-1]==player)
+		{
+			return true;
+		}
+		else if(lastColumnPlayed>=1&&lastColumnPlayed<=4&&
+				available[lastColumnPlayed]>=1&&available[lastColumnPlayed]<=6&&
+				board[lastColumnPlayed-1][available[lastColumnPlayed]-1]==player&&
+				board[lastColumnPlayed+2][available[lastColumnPlayed]-1]==player&&
+				board[lastColumnPlayed+1][available[lastColumnPlayed]-1]==player)
+		{
+			return true;
+		}
+		else if(lastColumnPlayed<=3&&lastColumnPlayed>=0&&
+				available[lastColumnPlayed]>=1&&available[lastColumnPlayed]<=6&&
+				board[lastColumnPlayed+1][available[lastColumnPlayed]-1]==player&&
+				board[lastColumnPlayed+2][available[lastColumnPlayed]-1]==player&&
+				board[lastColumnPlayed+3][available[lastColumnPlayed]-1]==player)
+		{
+			return true;
+		}
+		else if(lastColumnPlayed<=6&&lastColumnPlayed>=0&&
+				available[lastColumnPlayed]>=4&&available[lastColumnPlayed]<=6&&
+				board[lastColumnPlayed][available[lastColumnPlayed]-2]==player&&
+				board[lastColumnPlayed][available[lastColumnPlayed]-3]==player&&
+				board[lastColumnPlayed][available[lastColumnPlayed]-4]==player)
+		{
+			return true;
+		}
+		else if(available[lastColumnPlayed]>=4&&available[lastColumnPlayed]<=6&&
+				lastColumnPlayed>=3&&lastColumnPlayed<=6&&
+				board[lastColumnPlayed-1][available[lastColumnPlayed]-2]==player&&
+				board[lastColumnPlayed-2][available[lastColumnPlayed]-3]==player&&
+				board[lastColumnPlayed-3][available[lastColumnPlayed]-4]==player)
+		{
+			return true;
+		}
+		else if(lastColumnPlayed>=2&&lastColumnPlayed<=5
+				&&available[lastColumnPlayed]>=3&&available[lastColumnPlayed]<=5&&
+				board[lastColumnPlayed-1][available[lastColumnPlayed]-2]==player&&
+				board[lastColumnPlayed-2][available[lastColumnPlayed]-3]==player&&
+				board[lastColumnPlayed+1][available[lastColumnPlayed]]==player)
+		{
+			return true;
+		}
+		else if(lastColumnPlayed>=1&&lastColumnPlayed<=4&&
+				available[lastColumnPlayed]>=2&&available[lastColumnPlayed]<=4&&
+				board[lastColumnPlayed-1][available[lastColumnPlayed]-2]==player&&
+				board[lastColumnPlayed+2][available[lastColumnPlayed]+1]==player&&
+				board[lastColumnPlayed+1][available[lastColumnPlayed]]==player)
+		{
+			return true;
+		}
+		else if(lastColumnPlayed<=3&&available[lastColumnPlayed]<=3&&
+				board[lastColumnPlayed+1][available[lastColumnPlayed]]==player&&
+				board[lastColumnPlayed+2][available[lastColumnPlayed]+1]==player&&
+				board[lastColumnPlayed+3][available[lastColumnPlayed]+2]==player)
+		{
+			return true;
+		}
+		else if(lastColumnPlayed<=3&&available[lastColumnPlayed]>=4&&
+				board[lastColumnPlayed+1][available[lastColumnPlayed]-2]==player&&
+				board[lastColumnPlayed+2][available[lastColumnPlayed]-3]==player&&
+				board[lastColumnPlayed+3][available[lastColumnPlayed]-4]==player)
+		{
+			return true;
+		}
+		else if(lastColumnPlayed>=1&&lastColumnPlayed<=4&&
+				available[lastColumnPlayed]>=3&&available[lastColumnPlayed]<=5&&
+				board[lastColumnPlayed-1][available[lastColumnPlayed]]==player&&
+				board[lastColumnPlayed+2][available[lastColumnPlayed]-3]==player&&
+				board[lastColumnPlayed+1][available[lastColumnPlayed]-2]==player)
+		{
+			return true;
+		}
+		else if(lastColumnPlayed>=2&&lastColumnPlayed<=5&&
+				available[lastColumnPlayed]>=2&&available[lastColumnPlayed]<=4&&
+				board[lastColumnPlayed-1][available[lastColumnPlayed]]==player&&
+				board[lastColumnPlayed-2][available[lastColumnPlayed]+1]==player&&
+				board[lastColumnPlayed+1][available[lastColumnPlayed]-2]==player)
+		{
+			return true;
+		}
+		else if(available[lastColumnPlayed]<=3&&available[lastColumnPlayed]>=0&&
+				lastColumnPlayed>=3&&lastColumnPlayed<=6&&
+				board[lastColumnPlayed-1][available[lastColumnPlayed]]==player&&
+				board[lastColumnPlayed-2][available[lastColumnPlayed]+1]==player&&
+				board[lastColumnPlayed-3][available[lastColumnPlayed]+2]==player)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		} // DON'T FORGET TO CHANGE THE RETURN
 	}
 	
 	public int canWinNextRound (int player){
-		// ADD YOUR CODE HERE
-		return 0; // DON'T FORGET TO CHANGE THE RETURN
+		for(int index=0;index<7;index++)
+		{
+			if(available[index]<6)
+			{
+				board[index][available[index]]=player;
+				available[index]++;
+				if(isWinning(index,player)==false)
+				{
+					available[index]--;
+					board[index][available[index]]=0;
+					continue;
+				}
+				else if(isWinning(index,player)==true)
+				{
+					available[index]--;
+					board[index][available[index]]=0;
+					return index;
+				}
+			}
+			else 
+				{
+					continue;
+				}
+		}
+		return -1;
+		// ADD YOUR CODE HERE // DON'T FORGET TO CHANGE THE RETURN
 	}
 	
 	public int canWinTwoTurns (int player){
+		int i=0;int a=-1;int j=0;int num=0;int counter=0;
+		if(player==1)
+			i=2;
+		else if(player==2)
+			i=1;
+		for(int index=0;index<7;index++)
+		{
+			if(available[index]<6)
+			{
+				board[index][available[index]]=player;
+				available[index]++;
+				if(spaceLeft==true)
+				{
+					if(canWinNextRound(i)!=-1)
+					{
+						available[index]--;
+						board[index][available[index]]=0;
+						return -1;
+					}
+					else
+					{
+						for(j=0;j<7;j++)
+						{
+							if(available[j]<6)
+							{
+								board[j][available[j]]=i;
+								available[j]++;
+								if(counter==0)
+								{
+									for(int col=0;col<7;col++)
+									{
+										if(available[col]==6)
+										{
+											counter++;
+										}
+									}
+								}
+								if(spaceLeft==true)
+								{
+									if(canWinNextRound(player)!=-1)
+									{
+										num++;
+										available[j]--;
+										board[j][available[j]]=0;
+									}
+									else if (canWinNextRound(player)==-1)
+									{
+										available[j]--;
+										board[j][available[j]]=0;
+										break;
+									}
+									if(num==7-counter)
+									{
+										a=index;
+										break;
+									}
+								}
+							}
+							else if(available[j]==6)
+							{
+								continue;
+							}
+						}
+					}
+				}
+				if(a==-1)
+				{
+					available[index]--;
+					board[index][available[index]]=0;
+					continue;
+				}
+				else if(a!=-1)
+				{
+					available[index]--;
+					board[index][available[index]]=0;
+					break;
+				}
+			}
+			else if(available[index]==6)
+			{
+				continue;
+			}
+		}
 		// ADD YOUR CODE HERE
-		return 0; // DON'T FORGET TO CHANGE THE RETURN
+		return a; // DON'T FORGET TO CHANGE THE RETURN
 	}
-	
+
 }
--- assignment4/assignment4Game/Game.java
+++ assignment4/assignment4Game/Game.java
@@ -1,5 +1,5 @@
 package assignment4Game;
-
+//Name: Hanwen Wang   ID:260778557
 import java.io.*;
 
 public class Game {
@@ -21,7 +21,6 @@ public class Game {
 			if (player == 1){
 				columnPlayed = movePlayer1(columnPlayed, c);
 			}
-			System.out.println(columnPlayed);
 			c.addDisk(columnPlayed, player);
 			if (c.isWinning(columnPlayed, player)){
 				c.print();
@@ -34,8 +33,28 @@ public class Game {
 	}
 	
 	public static int getNextMove(BufferedReader keyboard, Configuration c, int player){
+		try
+		{
+			System.out.print("Enter a column number:");
+			InputStreamReader input1 = new InputStreamReader(System.in);
+			keyboard=new BufferedReader(input1);
+			int column=keyboard.read()-48;
+			while(c.available[column]==6)
+			{
+				System.out.print("Enter a column number again:");
+				InputStreamReader input = new InputStreamReader(System.in);
+				keyboard=new BufferedReader(input);
+				column=keyboard.read()-48;
+			}
+			return column;
+		}
+		catch(Throwable e)
+		{
+			System.out.println("Wrong input number!");
+		}
+		return -1;
 		// ADD YOUR CODE HERE
-		return 0; // DON'T FORGET TO CHANGE THE RETURN
+		// DON'T FORGET TO CHANGE THE RETURN
 	}
 	
 	public static int firstMovePlayer1 (){
@@ -43,6 +62,36 @@ public class Game {
 	}
 	
 	public static int movePlayer1 (int columnPlayed2, Configuration c){
+		if(c.canWinNextRound(1)!=-1)
+		{
+			return c.canWinNextRound(1);
+		}
+		else if(c.canWinTwoTurns(1)!=-1)
+		{
+			return c.canWinTwoTurns(1);
+		}
+		else
+		{
+			if(c.available[columnPlayed2]<=5)
+			{
+				return columnPlayed2;
+			}
+			else
+			{
+				for(int i=1;i<6;i++)
+				{
+					if(columnPlayed2-i>=0&&c.available[columnPlayed2-i]<=5)
+					{
+						return columnPlayed2-i;
+					}
+					else if(columnPlayed2+i<=6&&c.available[columnPlayed2+i]<=5)
+					{
+						return columnPlayed2+i;
+					}
+					else continue;
+				}
+			}
+		}
 		// ADD YOUR CODE HERE
 		return 0; // DON'T FORGET TO CHANGE THE RETURN
 	}
